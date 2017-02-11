398953a15e500bc23381ccc636791b96d33acf19
9schurke9 <schurke9@hotmail.de>
2017-02-11 01:09:08 +0100

    -
    -

--- Rekursion/exponentielleGlaettung.java
+++ Rekursion/exponentielleGlaettung.java
@@ -1,14 +1,13 @@
 public class exponentielleGlaettung{
 	
 	public double smoothing(double value){
-		double smoothValue = .0;
+		double smoothValue = .0; 
 
 		double last;
 		double current = smoothValue *value +(1-smoothValue))*last;	
-		last = current;
-		smoothValue = last;
+		smoothValue = current;
 
-		return smoothValue
+		return smoothValue;
 	}
 
 
