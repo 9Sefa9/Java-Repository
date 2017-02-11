9a61308bbe2b5e643e394f23b7c66be92770fdce
Sefa Notebook <sefagoevercin@hotmail.de>
2017-02-11 22:57:29 +0100

    -
    -

--- Rekursion/exponentielleGlaettung.java
+++ Rekursion/exponentielleGlaettung.java
@@ -1,14 +1,25 @@
 public class exponentielleGlaettung{
 	
-	public double smoothing(double value){
+double smoothing(double value){
+		
+
 		double smoothValue = .0; 
 
-		double last;
-		double current = smoothValue *value +(1-smoothValue))*last;	
-		smoothValue = current;
-
+		double alpha = 1.0;
+		value = smoothValue*value+(1-alpha);	
+		smoothValue = value;
 		return smoothValue;
+	
+
+
+
+
 	}
+
+
+
+
+
 
 
 
