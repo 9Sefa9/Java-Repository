2dff0d297572e239d48a42c266b4c3c430c60a48
Sefa Notebook <sefagoevercin@hotmail.de>
2017-02-23 15:03:07 +0100

    OOP
    OOP ist fertig.

--- OOPScripts/HilfsKlasse.java
+++ OOPScripts/HilfsKlasse.java
@@ -3,11 +3,24 @@ public class HilfsKlasse{
 		//Auto mercedes;
 		//mercedes = new Auto(300,5,4);
 		//System.out.println(mercedes);
-		Automarke bla = new Automarke("mitshubishi",300,4,5);
-		System.out.println(bla);
-	
+		
+
+		//Automarke bla = new Automarke("mitshubishi",300,4,5);
+		//System.out.println(bla);
+		
+
 		//Autofarbe erfundenefarbe = new Autofarbe();
 		//erfundenefarbe.mischen("weiß","rot");
 		//erfundenefarbe.ausstattung();
+		
+
+		//Auto x = new Automarke("porsche",360,5,2);
+		//Auto y = new Autofarbe();
+		//System.out.println(x);
+		//y.setPsstaerke(20);
+		//System.out.print(y.getPsstaerke());
+		//((Automarke)x).setAutomarke("porsche");
+		//System.out.println(((Automarke)x).getAutomarke());
+		
 	}
 }
