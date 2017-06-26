d878fb67919a71938a8a2da59d2975858195bed9
Sefa Laptop <sefagoevercin@hotmail.de>
2017-06-26 19:14:34 +0200

    objekt referenz verstanden!

---
+++ Test/ObjektReferenz.java
@@ -0,0 +1,15 @@
+public class ObjektReferenz{
+
+	
+	public static void main(String[]arg){
+
+	}	
+	public static void wahr(){
+		ObjektReferenz test;
+		test.falsch();
+		System.out.println();
+	}
+	public boolean falsch(){
+		return false;
+	}
+}
