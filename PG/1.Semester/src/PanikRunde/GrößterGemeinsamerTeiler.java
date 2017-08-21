18d6f86aa36533cd45a2a6a525b0789a8fec2ea3
Sefa Laptop <sefagoevercin@hotmail.de>
2017-08-21 14:02:56 +0200

    Bitte Splitsort hinzufügen!

--- PG/1.Semester/src/PanikRunde/GrößterGemeinsamerTeiler.java
+++ PG/1.Semester/src/PanikRunde/GrößterGemeinsamerTeiler.java
@@ -4,7 +4,7 @@ public class GrößterGemeinsamerTeiler {
 
 	public static void main(String[] args) {
 		Scanner eingabe = new Scanner(System.in);
-		System.out.println("größter gemeinsamer teiler: " + ggt(8964,5063));
+		System.out.println("größter gemeinsamer teiler: " + ggt(624,7));
 	}
 
 	public static int ggt(int a, int b) {
