048ac294b524d482714edde3e2ca88ad00ee94d2
Sefa Laptop <sefagoevercin@hotmail.de>
2017-09-11 21:38:19 +0200

    committing server and client for rraspb.

--- Client.java
+++ Client.java
@@ -6,7 +6,7 @@ public class Client {
     Socket client;
     public static void main(String[] args) throws IOException {
             Client c = new Client();
-            c.client = new Socket("192.168.178.75",80);
+            c.client = new Socket("rudralovesparo.ddns.net",25);
         try(DataOutputStream dos = new DataOutputStream(c.client.getOutputStream())){
             dos.writeUTF("HIII");
         }catch (IOException i){
