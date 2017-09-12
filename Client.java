1837dc76889685c23b1295b8a2ce05871e02868b
Sefa Computer <sefagoevercin@hotmail.de>
2017-09-12 11:51:02 +0200

    -
    -

--- Client.java
+++ Client.java
@@ -6,7 +6,7 @@ public class Client {
     Socket client;
     public static void main(String[] args) throws IOException {
             Client c = new Client();
-            c.client = new Socket("rudralovesparo.ddns.net",25);
+            c.client = new Socket("rudralovesparo.ddns.net",31213);
         try(DataOutputStream dos = new DataOutputStream(c.client.getOutputStream())){
             dos.writeUTF("HIII");
         }catch (IOException i){
