ada8fb6bf6b19eedfccb7ab5951b5db4a14ba5f3
9schurke9 <sefagoevercin@hotmail.de>
2018-09-23 12:07:28 +0200

    -
    -

--- TestKlasse.java
+++ TestKlasse.java
@@ -1,19 +1,35 @@
 public class TestKlasse {
-    public static void main(String[] args){
-      System.out.println(f(10,42));
-    }
-    public static int f(int n,int x){
-      int n2 = n;
-      while(n2>0){
-           System.out.println(n2);
-            n2-=1;
-            
-          if((x-Math.pow(2,n))<=0){
-              return 0;
-          }
-           
-          
+      private static int startTime;
+      private static int endTime;
+      private static int frameTimes = 0;
+      private static short frames = 0;
+      public static void main(String[] ar){
+        StopAndPost();
       }
-      return -1;
-    }
-}
+      /** Start counting the fps**/
+      public final static void StartCounter()
+      {
+          //get the current time
+          startTime = 60000000;
+      }
+
+      /**stop counting the fps and display it at the console*/
+      public final static void StopAndPost()
+      {
+          //get the current time
+          endTime = (int) System.currentTimeMillis();
+          //the difference between start and end times
+          frameTimes = frameTimes + endTime - startTime;
+          //count one frame
+          ++frames;
+          //if the difference is greater than 1 second (or 1000ms) post the results
+          if(frameTimes >= 1000)
+          {
+              //post results at the console
+              System.out.println("FPS: "+frames);
+              //reset time differences and number of counted frames
+              frames = 0;
+              frameTimes = 0;
+          }
+      }
+  }
