09c912eecdf0e773973e9acc90b65bba49ede1d0
9Sefa9 <sefagoevercin@hotmail.de>
2020-05-29 16:53:29 +0200

    primzahl.java

--- Primzahl.java
+++ Primzahl.java
@@ -1,45 +1,20 @@
-package ÜbungenLösungen;
 import java.util.Scanner;
 public class Primzahl {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int a = sc.nextInt();
-		boolean isPrime = true;
-
-		/*
-		 * Nun verwenden wir eine Schleife, die von 1 bis a laeuft, um jede
-		 * dieser Zahlen auf Teilerfremdheit mit a zu ueberpruefen. Wenn die
-		 * Modulo-Operation der Zahl a mit dem momentanen Wert der
-		 * Schleifenvariable i null ergibt, dann ist i ein Teiler von a. Der
-		 * Zaehler fuer die Anzahl der Teiler wird dann inkrementiert.
-		 */
-
-		for (int i = 2; i < a; i++) {
-			if (a % i == 0) {
-				isPrime = false;
-				System.out.println(isPrime);
-				
-			}
-		}
-		
-		/*
-		 * Nun wird ausgegeben, ob es sich um eine Primzahl handelt.
-		 */
-
-		if( isPrime )
-			System.out.println(isPrime);
-
-		/*
-		 * Nota bene: Hier wurde angenommen, dass die 1 keine Primzahl ist, da
-		 * eine Primzahl per definitionem exakt zwei Teiler hat. Moechte man auch
-		 * die 1 als Primzahl erkannt bekommen aendert man obige Zeile zu:
-		 * System.out.println(teiler <= 2);
-		 * Ferner beachte man, dass in der Klammer, die fuer die Konsolenausgabe
-		 * verantwortlich ist, ein boolscher Ausdruck (Vergleich) steht.
-		 */
-
+public static boolean prim = true; 
+public static void main(String[] args) { 
+	  Scanner input = new Scanner(System.in);
+	  System.out.println("n Eingeben : ");
+	  long x = input.nextLong();
+	  primzahlTest(x);
+	  System.out.println(x+" ist " +(prim==true ? "eine Primzahl!" : "keine Primzahl!")); 
+	  input.close(); } 
+	  
+	  public static void primzahlTest(long x){ 
+		  for(long i = 2; i < x; ++i){ 
+			  if(x % i == 0){ 
+				  prim = false; 
+				  System.out.println("Teiler: "+i); 
+				} 
+			} 
+		} 
 	}
-
-}
