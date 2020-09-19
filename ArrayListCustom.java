4a479cd4f1cbe1cddacefba8ebe7461dc18678b5
9schurke9 <sefagoevercin@hotmail.de>
2020-09-19 18:30:36 +0200

    ..

--- ArrayListCustom.java
+++ ArrayListCustom.java
@@ -1,32 +1,43 @@
 public class ArrayListCustom<T>{
 
 private T[] list;
+int i;
 public static void main(String[] args){
-    ArrayListCustom<int> alc = new ArrayListCustom();
+    ArrayListCustom<Integer> alc = new ArrayListCustom();
 }
 public ArrayListCustom(){
-    this.list = new T[0];
+    this.list = (T[] )new T[0];
+    i=0;
 }
 
 public void add(T element){
-    resize();
+    resizeUp();
+    list[i] = element;
 
 }
 public T get(int index){
+    return list[index];
+}
+public int indexOf(T element){
+    for(int i=0; i<this.list.length;i++){
+        if(this.list[i].equals(element)){
+            return i;
+        }
+    }
 
 }
-public int indexOf(){
-
-
-}
-public void sort(){
-
+public void print(){
+    for(int i=0; i<this.list.length;i++){
+        System.out.println(this.list[i]);
+    }
 }
 private void resizeUp(){
     T[] temp = new T[this.list.length+1];
-    for(int i = 0; i<this.list.length; i++){
-        temp[i] = this.list[i];
+    for(int i = 0; i<temp.length; i++){
+        if(i!=this.list.length)
+            temp[i] = this.list[i];
     }
+    this.list = temp;
 
 }
 private void resizeDown(){
